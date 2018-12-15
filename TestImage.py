c746cd8aace1f850e24ab93f5d412278c2300de9
Indranil Guha <indranil.guha21@gmail.com>
2018-12-14 23:05:27 -0600

    Update TestImage.py

--- TestImage.py
+++ TestImage.py
@@ -4,5 +4,5 @@ import matplotlib.pyplot as plt
 
 unet = load_model('unet-original.keras')
 
-CustomImage = 'TOY.jpg'
+CustomImage = 'SNR-12-Data.jpg'
 plt.imshow(predict_custom_image(CustomImage, model=unet))
