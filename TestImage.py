6db254eb8bc597a9d3646d45e10ecee147e8d31c
Indranil Guha <indranil.guha21@gmail.com>
2018-12-14 23:03:13 -0600

    Update TestImage.py

--- TestImage.py
+++ TestImage.py
@@ -2,9 +2,7 @@ from PreprocessImage import *
 from keras.models import load_model
 import matplotlib.pyplot as plt
 
-#unet = load_model('unet-original.keras')
-unet = load_model('unet3.keras')
-
+unet = load_model('unet-original.keras')
 
 CustomImage = 'TOY.jpg'
 plt.imshow(predict_custom_image(CustomImage, model=unet))
