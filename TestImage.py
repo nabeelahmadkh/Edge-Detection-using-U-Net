270edcd4f10327f4763d883d3edaf02be0dda0ab
Indranil Guha <indranil.guha21@gmail.com>
2018-12-14 23:06:24 -0600

    Update TestImage.py

--- TestImage.py
+++ TestImage.py
@@ -5,4 +5,5 @@ import matplotlib.pyplot as plt
 unet = load_model('unet-original.keras')
 
 CustomImage = 'SNR-12-Data.jpg'
-plt.imshow(predict_custom_image(CustomImage, model=unet))
+c=predict_custom_image(CustomImage, model=unet)
+imsave('edges.jpg', c)
